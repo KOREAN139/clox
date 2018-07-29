0756a6d8877178304192776cd44eb8f92b752d2b
KOREAN139 <korean139@gmail.com>
2018-07-30 02:59:33 +0900

    Implement heap initialize & destruct
    Implement heap initialization & destruction with mmap() in sys/mman.h
    This is for hardcore mode in chapter 14 - challenge 3
    
    Challenge 3. Hardcore mode
    http://www.craftinginterpreters.com/chunks-of-bytecode.html#challenges

--- memory.c
+++ memory.c
@@ -1,7 +1,43 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <sys/mman.h>
 
 #include "common.h"
 #include "memory.h"
+
+/* Request 4MB memory pool from OS, and let memory_pool point the pool */
+static void __attribute__((constructor)) init_heap()
+{
+  memory_pool = mmap(NULL, MAX_HEAP_SIZE, (PROT_READ | PROT_WRITE),
+                     (MAP_PRIVATE | MAP_ANONYMOUS), -1, 0);
+
+  if (memory_pool == (void *) -1) {
+    puts("FAILURE ON MMAP()");
+    exit(EXIT_FAILURE);
+  }
+
+  /* (16 * sizeof(struct block_meta *))bytes is for freed_block_list */
+  freed_block_list = (struct block_meta **)memory_pool;
+  brk_ptr = (char *)memory_pool + 16 * sizeof(struct block_meta *);
+}
+
+/* Give back borrowed 4MB memory pool to OS */
+static void __attribute__((destructor)) destroy_heap()
+{
+  /*
+   * Once mmap() successfully give us memory pool,
+   * the pool size and address of pool are never modified through program
+   * Thus munmap() never fails if this program ends normally
+   */
+  munmap(memory_pool, MAX_HEAP_SIZE);
+}
+
+/* Do implement */
+void __free(void *block);
+
+void *__malloc(size_t size);
+
+void *__realloc(void *block, size_t old, size_t update);
 
 void *reallocate(void *previous, size_t old, size_t update)
 {
--- memory.h
+++ memory.h
@@ -11,6 +11,33 @@
 #define FREE_ARRAY(type, ptr, old)                       \
         reallocate(ptr, sizeof(type) * (old), 0)
 
+#define MAX_HEAP_SIZE 4 * 1024 * 1024
+
+/* Points memory pool which we use as Heap */
+static void *memory_pool;
+
+/* Points very first address of memory which isn't used yet in pool */
+static char *brk_ptr;
+
+/*
+ * Metadata of block
+ * Size of block = (2^sz) bytes
+ * next points next freed block if any
+ * block is returned when {m,re}alloc is called
+ */
+struct block_meta{
+  size_t sz;
+  struct block_meta *next;
+  void *block;
+};
+#define BLOCK_META_SIZE (sizeof(struct block_meta))
+
+/*
+ * List of freed blocks
+ * Block within n-th list has (2^n)byte size
+ */
+static struct block_meta **freed_block_list;
+
 /*
  * old       | update    | operation
  * 0         | Non-zero  | Allocate new block
